1a3395480c7a8cf67fcfc3fb3792cce5a365bb73
Nachtalb <info@nachtalb.io>
2022-07-25 16:09:54 +0200

    Disable right clicking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  if (process.browser) {
+    // Disable right clicking
+    document.addEventListener('contextmenu', event => event.preventDefault());
+  }
   return <Component {...pageProps} />;
 }
 
