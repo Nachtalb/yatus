27a040954c75cacb6cffdd2ca56c71e412c00554
Nachtalb <info@nachtalb.io>
2022-07-25 14:46:22 +0200

    Initial Commit

---
+++ pages/dashboard.tsx
@@ -0,0 +1,99 @@
+import Head from "next/head";
+import React from "react";
+import styles from "../styles/Home.module.css";
+import dashboardStyles from "../styles/Dashboard.module.css";
+
+function Input({ name, label, errorState, icon }) {
+  const [error, _] = errorState;
+  return (
+  <div className="input">
+    <img className="icon" src={icon} alt={label} />
+    <label htmlFor={name}>
+      <span>{label}</span>
+      <input type="password" name={name} id={name} />
+      {error !== "" &&
+        <p className="error">{error}</p>
+      }
+    </label>
+  </div>
+  )
+}
+
+export default function Dashboard({}) {
+  const inputs = [
+  {
+    name: "passcode1",
+    label: "Passcode 1",
+    password: "420",
+    icon: "public/search.svg",
+    errorState: React.useState("")
+  },
+  {
+    name: "passcode2",
+    label: "Passcode 2",
+    password: "101835",
+    icon: "public/code.svg",
+    errorState: React.useState("")
+  },
+  {
+    name: "passcode3",
+    label: "Passcode 3",
+    password: "hund",
+    icon: "public/map-pin.svg",
+    errorState: React.useState("")
+  },
+  {
+    name: "passcode4",
+    label: "Passcode 4",
+    password: "watermelon",
+    icon: "public/feather.svg",
+    errorState: React.useState("")
+  },
+  ]
+
+  function formSubmit(event) {
+    event.preventDefault()
+    const data = new FormData(event.target)
+    inputs.map((object) => {
+      object.errorState[1]("")
+      const value = data.get(object.name)
+      if (value === "") {
+        object.errorState[1]("The passcode cannot be empty")
+      } else if (data.get(object.name) !== object.password) {
+        object.errorState[1]("The passcode is wrong")
+      }
+    })
+
+  }
+
+  return (
+    <div className={dashboardStyles.container}>
+      <Head>
+        <title>Dashboard - Tactical Watermelon Deployment System</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <h1 className={styles.title}>
+          <img src="public/watermelon.svg" alt="Tactical Watermelon"/>
+          Tactical Watermelon Deployment System
+        </h1>
+        <p className={styles.description}>
+          The best enterprise solution against any mammoth gone wild ~ Swiss Armed Forces
+        </p>
+      </header>
+
+      <main className={styles.main}>
+        <form action="#" onSubmit={formSubmit}>
+          {inputs.map((object) => {
+            return <Input {...object} key={object.name} />
+          })}
+
+          <div className="input">
+            <button type="submit">Deploy</button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Home({}) {
+
+  const pwd_input = React.createRef();
+  const error = React.createRef();
+  let [counter, setCounter] = React.useState(0)
+  let [showHint, setShowHint] = React.useState(false)
+
+  function formSubmit(event) {
+    event.preventDefault();
+    const pwd = pwd_input.current.value
+    pwd_input.current.value = ""
+    error.current.innerText = ""
+
+    if (pwd === "house") {
+      setCounter(0)
+      setShowHint(false)
+      window.open("/dashboard", "_self")
+    } else if (pwd === "") {
+      error.current.innerText = "Password must not be empty"
+    } else {
+      error.current.innerText = "Password is not correct"
+      setCounter(counter + 1)
+    }
+  }
+
+  function toggleHint() {
+    setShowHint(!showHint)
+  }
+
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tactical Watermelon Deployment System</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <h1 className={styles.title}>
+          <img src="public/watermelon.svg" alt="Tactical Watermelon"/>
+          Tactical Watermelon Deployment System
+        </h1>
+        <p className={styles.description}>
+          The best enterprise solution against any mammoth gone wild ~ Swiss Armed Forces
+        </p>
+      </header>
+
+      <main className={styles.main}>
+        <form action="#" onSubmit={formSubmit}>
+          <input type="password" name="password" ref={pwd_input} autoFocus/>
+          <p ref={error} className="error"></p>
+          {counter > 2 &&
+            <p><a onClick={toggleHint} href="#">Forgot Password?</a></p>
+          }
+          { showHint &&
+            <audio src="public/morse.mp3" controls></audio>
+          }
+        </form>
+      </main>
+    </div>
+  );
+}
