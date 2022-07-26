ac97c5fd9743ceb49949d18ab2008adcc7b69b1f
Nachtalb <info@nachtalb.io>
2022-07-26 19:45:03 +0200

    Prevent keycombinations to prevent cheating

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,17 @@ import "../styles/globals.css";
 import "../styles/Dashboard.css";
 
 function MyApp({ Component, pageProps }) {
-  if (process.browser) {
+  function keyHandler(event: KeyboardEvent) {
+    // Prevent F keys, ctrl and alt combinations
+    if ((event.keyCode >= 112 && event.keyCode <= 136) || event.ctrlKey || event.altKey) {
+      event.preventDefault()
+    }
+  }
+
+  if (typeof window !== 'undefined') {
     // Disable right clicking
     document.addEventListener('contextmenu', event => event.preventDefault());
+    document.addEventListener("keydown", keyHandler)
   }
   return <Component {...pageProps} />;
 }
